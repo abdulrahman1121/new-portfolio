d42445a2d03b2c184c3d9a8fdd513eb807d9dd90
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-11-13 15:18:01 -0800

    Update resume to Resume (27).pdf and remove old resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
           download
           target="_blank"
           rel="noopener noreferrer"
-          href="/Resume (26).pdf"
+          href="/Resume (27).pdf"
         >
           <IoDocumentTextOutline className="size-6 text-white hover:text-white/90 cursor-pointer" />
         </a>
